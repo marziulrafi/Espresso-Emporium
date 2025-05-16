cbcc30acb044d19ea9d494ebaa54c0a9dc5c756f
K M Marziul Karim <marziulrafi@gmail.com>
2025-05-16 20:35:36 +0600

    setup

---
+++ Espresso-Emporium-Client/src/Route/routes.jsx
@@ -0,0 +1,16 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello</div>
+    
+  },
+]);
+
+
+
+export default router
