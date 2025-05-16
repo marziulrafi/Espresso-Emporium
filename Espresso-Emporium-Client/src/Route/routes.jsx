be411dc4b0b6f725549a2729a6f528f672b58b3a
K M Marziul Karim <marziulrafi@gmail.com>
2025-05-16 21:21:39 +0600

    add coffee page

--- Espresso-Emporium-Client/src/Route/routes.jsx
+++ Espresso-Emporium-Client/src/Route/routes.jsx
@@ -1,14 +1,32 @@
 import {
-  createBrowserRouter,
-  RouterProvider,
+    createBrowserRouter,
+    RouterProvider,
 } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../components/Home";
+import AddCoffee from "../components/AddCoffee";
+import UpdateCoffee from "../components/UpdateCoffee";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello</div>
-    
-  },
+    {
+        path: "/",
+        Component: MainLayout,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: '/add-coffee',
+                Component: AddCoffee
+            },
+            {
+                path: '/update-coffee',
+                Component: UpdateCoffee
+            }
+        ]
+
+    },
 ]);
 
 
